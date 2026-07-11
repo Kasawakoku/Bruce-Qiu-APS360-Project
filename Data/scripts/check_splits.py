21d73fd619c4365227d7ee872d2a0312ec521627
Bruce <qiub0222@yahoo.com>
2026-07-11 02:41:44 -0400

    cleanup ongoing

--- Data/scripts/check_splits.py
+++ Data/scripts/check_splits.py
@@ -209,7 +209,7 @@ def analyze_skew():
 
 
 if __name__ == "__main__":
-    analyze_skew()
+    # analyze_skew()
     # Ensure you update the Configuration paths at the top of the file to match your CSV names.
 
     # Example 1: View the entire dataset split
@@ -221,7 +221,13 @@ if __name__ == "__main__":
     # visualize_split(airline_filter="Delta Air Lines")
     
     # Example 3: View split for a specific variant only
-    #visualize_split(variant_filter="ATR 72 (AT72/AT73/AT74/AT75/AT76)")
+    visualize_split(variant_filter="Boeing 737-300 (B733)")
+
+    visualize_split(airline_filter="Austrian Airlines")
+
+    visualize_split(airline_filter="OTHERS")
+
+    # visualize_split(variant_filter="Canadian Regional Jet CRJ-900/-705/Challenger 890 (CRJ9)")
     
     # Example 4: View split for a specific airline AND variant
     # visualize_split(airline_filter="Delta Air Lines", variant_filter="Boeing 737-800")
